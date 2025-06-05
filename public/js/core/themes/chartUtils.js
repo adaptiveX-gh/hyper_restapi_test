0202b936e469a74fd4fe72c886f331624b900f89
adaptiveX-gh <62356956+adaptiveX-gh@users.noreply.github.com>
2025-06-05 01:55:05 -0400

    fix theme update for highcharts

--- public/js/core/themes/chartUtils.js
+++ public/js/core/themes/chartUtils.js
@@ -18,27 +18,29 @@ export function applyThemeToCharts(theme) {
 
   window.Highcharts.charts.forEach(c => {
     if (!c) return;
-    c.update({
-      chart: {
-        backgroundColor: theme.chart?.backgroundColor,
-        style: theme.chart?.style
-      },
-      xAxis: [{
-        gridLineColor: theme.xAxis?.gridLineColor,
-        labels: { style: theme.xAxis?.labels?.style }
-      }],
-      yAxis: [{
-        gridLineColor: theme.yAxis?.gridLineColor,
-        labels: { style: theme.yAxis?.labels?.style }
-      }],
-      tooltip: {
-        backgroundColor: theme.tooltip?.backgroundColor,
-        style: theme.tooltip?.style
-      },
-      legend: {
-        itemStyle: { color: theme.legend?.itemStyle?.color }
-      }
-    }, false, false, false);
+    const updateOpts = { chart:{}, xAxis:[{}], yAxis:[{}], tooltip:{}, legend:{} };
+    if (theme.chart?.backgroundColor !== undefined)
+      updateOpts.chart.backgroundColor = theme.chart.backgroundColor;
+    if (theme.chart?.style) updateOpts.chart.style = theme.chart.style;
+
+    if (theme.xAxis?.gridLineColor !== undefined)
+      updateOpts.xAxis[0].gridLineColor = theme.xAxis.gridLineColor;
+    if (theme.xAxis?.labels?.style)
+      updateOpts.xAxis[0].labels = { style: theme.xAxis.labels.style };
+
+    if (theme.yAxis?.gridLineColor !== undefined)
+      updateOpts.yAxis[0].gridLineColor = theme.yAxis.gridLineColor;
+    if (theme.yAxis?.labels?.style)
+      updateOpts.yAxis[0].labels = { style: theme.yAxis.labels.style };
+
+    if (theme.tooltip?.backgroundColor !== undefined)
+      updateOpts.tooltip.backgroundColor = theme.tooltip.backgroundColor;
+    if (theme.tooltip?.style) updateOpts.tooltip.style = theme.tooltip.style;
+
+    if (theme.legend?.itemStyle?.color !== undefined)
+      updateOpts.legend.itemStyle = { color: theme.legend.itemStyle.color };
+
+    c.update(updateOpts, false, false, false);
     c.redraw(false);
   });
 }
